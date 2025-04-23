ba6b1c9f93605ed9f3b79912d7132daa4cddd16a
Qiuyan Xu <46363760+qyxu@users.noreply.github.com>
2025-04-23 19:49:59 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,23 +1,22 @@
-
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Job(Base):
-    __tablename__ = 'jobs'
+    __tablename__ = "jobs"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    job_id = Column(String, unique=True)
+    id = Column(Integer, primary_key=True, index=True)
+    job_id = Column(String, unique=True, index=True)
     duration = Column(Integer)
     due_date = Column(Integer)
     machine_required = Column(String)
     skill_required = Column(String)
 
 class Schedule(Base):
-    __tablename__ = 'schedules'
+    __tablename__ = "schedules"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True)
     job_id = Column(String)
     start = Column(Integer)
     end = Column(Integer)
